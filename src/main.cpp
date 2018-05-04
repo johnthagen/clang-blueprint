e48a83d39ad2214ef4574f1d0846746afd580049
johnthagen <johnthagen@gmail.com>
2018-05-04 14:23:49 -0700

    Correctly print new line on output

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,6 @@ int32_t main(const int32_t argc, const char* argv[]) {
     const int32_t n = 7;
     const int32_t result = fact::factorial(n);
 
-    std::cout << "Hello, World! factorial(" << n << ") = " << result;
+    std::cout << "Hello, World! factorial(" << n << ") = " << result << std::endl;
     return EXIT_SUCCESS;
 }
