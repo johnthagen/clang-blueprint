7b320f1402390539f915b4b699cb54a03dc526a1
johnthagen <johnthagen@gmail.com>
2018-05-07 14:53:26 -0700

    Fix failing test when no AddressSanitizer is used

--- test/danger/test_bad_examples.cpp
+++ test/danger/test_bad_examples.cpp
@@ -9,7 +9,5 @@ int danger_use_after_free_function(const int32_t index) {
 }
 
 TEST_CASE("Intentional AddressSanitizer bug", "[danger]") {
-    int32_t a = danger_use_after_free_function(3);
-
-    REQUIRE(a == 0);
+    danger_use_after_free_function(3);
 }
