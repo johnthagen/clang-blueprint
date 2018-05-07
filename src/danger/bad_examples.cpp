02790fb32e294ef59e9adb39b160370760b36c40
johnthagen <johnthagen@gmail.com>
2018-05-07 14:32:48 -0700

    Move Cppcheck violating example to a new danger namespace

---
+++ src/danger/bad_examples.cpp
@@ -0,0 +1,16 @@
+/// \file
+/// File that contains known bugs to demonstrate analyzer behavior.
+
+#include <iostream>
+
+namespace danger {
+
+/// A memory unsafety bug designed to be detected by Cppcheck.
+/// \warning This code has an intentional bug and should not be used as an example.
+void bad_cppcheck_example() {
+    char a[2] = {};
+
+    std::cout << a[3];
+}
+
+} // namespace danger
