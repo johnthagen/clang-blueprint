fc007a11e313504a6b2676c057120c8446218b4c
johnthagen <johnthagen@gmail.com>
2018-05-07 14:44:51 -0700

    Use fixed width integers in test function

--- test/danger/test_bad_examples.cpp
+++ test/danger/test_bad_examples.cpp
@@ -1,13 +1,15 @@
+#include <cstdint>
+
 #include "catch2/catch.hpp"
 
-int danger_use_after_free_function(int argc) {
-    int* array = new int[100];
+int danger_use_after_free_function(const int32_t index) {
+    int32_t* array = new int32_t[100];
     delete[] array;
-    return array[argc];
+    return array[index];
 }
 
 TEST_CASE("Intentional AddressSanitizer bug", "[danger]") {
-    int a = danger_use_after_free_function(3);
+    int32_t a = danger_use_after_free_function(3);
 
     REQUIRE(a == 0);
 }
